0300f217de8029c96985d3bf17b64ad88cd89e78
Zach Chaney <zachary.chaney@gmail.com>
2015-05-21 14:51:34 -0500

    initial commit

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+
+
+//create a function to change the color of the div with the changeColor ID
+
+function changingColor(color) {
+	document.getElementById("changeColor").style.backgroundColor = color;
+}
+
+changingColor("#464646");
